d4a4630f89028b1e05c0f63cc4c42bb3f39606b5
mac <mac@macdeiMac.local>
2018-04-04 19:56:43 +0800

    聊天室

--- nodeJS/wxlite/app.js
+++ nodeJS/wxlite/app.js
@@ -1,17 +1,22 @@
 //app.js
 
 var qcloud = require('./lib/index');
+var util = require('./myutils/util.js')
+var config = require('./myutils/config.js')
+var subscriber = require('./myutils/event.js')
 
 App({
+
   onLaunch: function () {
     // 展示本地存储能力
     // qcloud.setLoginUrl(config.url + 'getwxinfo');
     // qcloud.setLoginUrl(config.url + 'login');
-
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res);
+        
       }
     })
     // 获取用户信息
@@ -29,6 +34,7 @@ App({
               if (this.userInfoReadyCallback) {
                 this.userInfoReadyCallback(res)
               }
+
             }
           })
         }else{
@@ -54,6 +60,7 @@ App({
     })
   },
 
+
   editTabBar: function () {
     var tabbar = this.globalData.tabbar,
       currentPages = getCurrentPages(),
@@ -70,6 +77,21 @@ App({
   },
 
   globalData: {
+    isLogin: false, // 当前是否是登录状态
+    currentChatTo: '', // 记录当前聊天对象account，用于标记聊天时禁止更新最近会话unread
+    loginUser: {},//当前登录用户信息
+    friends: [],//好友列表，
+    friendsWithCard: {}, // 带有名片信息的好友列表（转发时使用）
+    friendsCard: {},//带有名片信息的好友列表
+    onlineList: {},//在线人员名单 account: status
+    blackList: {},//黑名单列表
+    config,//存储appkey信息
+    nim: {},//nim连接实例
+    subscriber, //消息订阅器
+    notificationList: [], // 通知列表
+    recentChatList: {},//最近会话列表
+    rawMessageList: {}, //原生的所有消息列表(包含的字段特别多)
+    messageList: {},//处理过的所有的消息列表
     userInfo: null,
     tabbar: {
       color: "#000000",
